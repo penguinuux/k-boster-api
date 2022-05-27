de0052dca53e17fad067a4ccf537687f341f402d
Pablo Adriano <pabloadrianomc@gmail.com>
2022-05-27 09:11:32 -0300

    feat: modify create dvd schema

--- src/schemas/dvd/createDvd.schema.ts
+++ src/schemas/dvd/createDvd.schema.ts
@@ -1,9 +1,18 @@
 import * as yup from "yup";
 
-const createDvdSchema = yup.object().shape({
-  name: yup.string().required(),
-  duration: yup.string().required(),
-});
+const createDvdSchema = yup
+  .object()
+  .shape({
+    dvds: yup.array().of(
+      yup.object().shape({
+        name: yup.string().required(),
+        duration: yup.string().required(),
+        quantity: yup.number().required(),
+        price: yup.number().required(),
+      })
+    ),
+  })
+  .required();
 
 const serializedCreatedDvdSchema = yup.object().shape({
   dvd_id: yup.string().uuid().required(),
