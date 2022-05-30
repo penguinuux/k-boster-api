88fe87c6e75556f25d653063333af210e04a61f5
Pablo Adriano <pabloadrianomc@gmail.com>
2022-05-30 20:25:17 -0300

    feat: create heroku Procfile
    Modified package.json file;
    Modified data-source file;
    Inserted dist directory in .gitignore file.

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { AppDataSource } from "./data-source";
 AppDataSource.initialize()
   .then(() => {
     console.log("Database connected!");
-    const port = process.env.RUN_PORT ?? 3000;
+    const port = process.env.PORT ?? 3000;
 
     app.listen(port, () => {
       console.log(`App running on http://localhost:${port}/`);
