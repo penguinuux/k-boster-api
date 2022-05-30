5fdbee9258d2f8cb5d8a066355f17c06cada03cf
Pablo Adriano <pabloadrianomc@gmail.com>
2022-05-30 20:49:41 -0300

    fix: modify data-source and .env.example file

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,11 +6,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PWD,
-  database: process.env.POSTGRES_DB,
+  url: process.env.DATABASE_URL,
   logging: false,
   ssl: { rejectUnauthorized: false },
   entities: [path.join(__dirname, "./entities/**/*.{ts,js}")],
